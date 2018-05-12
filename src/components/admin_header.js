3bb5f076e49abb14e21d11ccd910f5e01332b7a2
munam <tmunam@hotmail.com>
2018-05-12 15:15:02 +0500

    test change

--- src/components/admin_header.js
+++ src/components/admin_header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class AdminHeader extends Component {
     render() {
         return (
-            <div>Header {React.version}</div>
+            <div>Header react version is: {React.version}</div>
         );
     }
 }
