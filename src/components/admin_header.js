78185b2d1c99519ebd217140848109f2318c0ff7
munam <tmunam@hotmail.com>
2018-05-12 13:36:17 +0500

    code

---
+++ src/components/admin_header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class AdminHeader extends Component {
+    render() {
+        return (
+            <div>Header {React.version}</div>
+        );
+    }
+}
+
+export default AdminHeader;
